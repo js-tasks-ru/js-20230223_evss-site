21b7983024243f1d7a4bb9014533aa257bb38128
e-free <e-free@mail.ru>
2023-03-05 13:32:58 +0800

    3-е задание

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,19 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  const arr = path.split('.');
 
+  let getter = (data) => {
+    let property = arr.shift();
+
+    if (!arr.length) {
+      return data[property];
+    } 
+    else {
+      const { [property]: subObject } = data;
+      return subObject === undefined ? subObject : getter(subObject);
+    }
+  };
+
+  return getter;
 }
--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,30 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  if (size === undefined) {
+    return string;
+  }
+  if (size < 1) {
+    return "";
+  }
 
+  let result = "";
+  let previousItem = "";
+  let count = 0;
+
+  for (let i = 0; i < string.length; i++) {
+    const currentItem = string[i];
+    if (currentItem != previousItem) {
+      result += currentItem;
+      previousItem = currentItem;
+      count = 0;
+    } 
+    else {
+      if (count < size - 1) {
+        result += string[i];
+        count++
+      }
+    }
+  }
+  return result;
 }
